01724dc16f4ea4444dae3305aa423f1a3dea62c7
ITGS_AD\yago.silva <yago.silva@onlineapp.com.br>
2024-12-04 08:20:37 -0300

    Added the code of days 1, 2 and 3

---
+++ Dia1/Program.cs
@@ -0,0 +1,53 @@
+﻿List<int> listaUm = [];
+List<int> listaDois = [];
+
+if (!File.Exists("input"))
+    throw new InvalidOperationException("File not found.");
+
+var input = await File.ReadAllLinesAsync("input");
+
+Console.Write($"QUAL PARTE DO DESAFIO QUER RESOLVER? 1 OU 2? ");
+var selecao = Console.ReadLine();
+
+MontarListas(listaUm, listaDois, input);
+
+if (selecao == "1") 
+    ParteUm(listaUm, listaDois);
+else if (selecao == "2") 
+    ParteDois(listaUm, listaDois);
+
+static void ParteUm(List<int> listaUm, List<int> listaDois)
+{
+    var um = listaUm.OrderByDescending(l => l).ToList();
+    var dois = listaDois.OrderByDescending(l => l).ToList();
+
+    var diferencas = um.Zip(dois, (um, dois) => Math.Abs(um - dois)).ToList();
+
+    System.Console.WriteLine($"SOMA DA DIFERENÇA = {diferencas.Sum()}");
+}
+
+static void ParteDois(List<int> listaUm, List<int> listaDois)
+{
+    var similaridade = 0;
+    foreach(var numero in listaUm)
+    {
+        var numRepeticao = listaDois.Where(l => l == numero).Count();
+        similaridade += numero * numRepeticao;
+    }
+
+    System.Console.WriteLine($"SCORE DE SIMILARIDADE = {similaridade}");
+}
+
+static void MontarListas(List<int> listaUm, List<int> listaDois, string[] input)
+{
+    foreach (var linhas in input)
+    {
+        var divisaoListas = linhas.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        
+        if (divisaoListas.Length == 2)
+        {
+            listaUm.Add(int.Parse(divisaoListas[0]));
+            listaDois.Add(int.Parse(divisaoListas[1]));
+        }
+    }
+}
---
+++ Dia2/Program.cs
@@ -0,0 +1,72 @@
+﻿var relatorios = File.ReadAllLines("input").ToList();
+
+Console.Write($"QUAL PARTE DO DESAFIO QUER RESOLVER? 1 OU 2? ");
+var selecao = Console.ReadLine();
+
+if (selecao == "1")
+    ParteUm(relatorios);
+else if (selecao == "2")
+    ParteDois(relatorios);
+
+static void ParteUm(List<string> relatorios)
+{
+    int relatoriosSeguros = 0;
+
+    foreach (var linhaRelatorio in relatorios)
+    {
+        var relatorio = linhaRelatorio.Split(' ').Select(int.Parse).ToList();
+
+        if (ValidarRelatorios(relatorio))
+            relatoriosSeguros++;
+    }
+
+    Console.WriteLine($"TOTAL RELATORIOS SEGUDOS: {relatoriosSeguros}");
+}
+
+static void ParteDois(List<string> relatorios)
+{
+    int relatoriosSeguros = 0;
+
+    foreach (var linhaRelatorio in relatorios)
+    {
+        var relatorio = linhaRelatorio.Split(' ').Select(int.Parse).ToList();
+
+        if (SeguroComReducaoDoNivel(relatorio))
+            relatoriosSeguros++;
+    }
+
+    Console.WriteLine($"TOTAL RELATORIOS SEGUDOS: {relatoriosSeguros}");
+}
+
+static bool ValidarRelatorios(List<int> relatorio)
+{
+    //List<int> diferencas = [];
+    // for (int i = 0; i < relatorio.Count - 1; i++)
+    //     diferencas.Add(relatorio[i] - relatorio[i + 1]);
+
+    // O SKIP CRIA UMA LISTA DO RELATORIO, PORÉM PULANDO O PRIMERIO INDICE
+    // APOS ISSO O ZIP COMPARA O PRIMEIRO ELEMENTO DE RELATORIO COM O PRIMEIRO DA LISTA CRIADA PELO SKIP
+    // COM ISSO RETORNANDO A DIFERENCA ENTRE OS DOIS
+    var diferencas = relatorio
+        .Zip(relatorio.Skip(1), (numRelatorio, numSkip) => numRelatorio - numSkip)
+        .ToList();
+
+    var ordenado = diferencas.All(d => d > 0) || diferencas.All(d => d < 0);
+
+    var validas = diferencas.All(d => Math.Abs(d) >= 1 && Math.Abs(d) <= 3);
+
+    return ordenado && validas;
+}
+
+static bool SeguroComReducaoDoNivel(List<int> relatorio)
+{
+    for (int i = 0; i < relatorio.Count; i++)
+    {
+        var relatorioReduzido = new List<int>(relatorio);
+        relatorioReduzido.RemoveAt(i);
+
+        if (ValidarRelatorios(relatorioReduzido))
+            return true;
+    }
+    return false;
+}
